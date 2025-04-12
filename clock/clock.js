7eb66f1787ee775a83d21d7c9f4024df563c92a8
Teja@git <125871752+TejaDev07@users.noreply.github.com>
2025-04-12 14:47:56 +0530

    Add files via upload

---
+++ clock/clock.js
@@ -0,0 +1,18 @@
+const clock=document.querySelector('.clock');
+
+const tick = () =>{
+
+    const now = new Date();
+
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+    const html = `
+     <span>${h}:${m}:${s} </span>
+    `;
+    // console.log(h,m,s);
+    
+    clock.innerHTML = html;
+}
+setInterval(tick,1000);
